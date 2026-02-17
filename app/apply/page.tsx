99797b2b2edb21040382fb0daf7b182373f2cefd
Alex <muchenealex24@gmail.com>
2026-02-17 09:50:13 +0300

    btn

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -202,6 +202,7 @@ export default function AdmissionsForm() {
         {/* Submit */}
         <button
           type="submit"
+          onClick={() => console.log("✅ button clicked")}
           className=" w-full bg-blue-600 text-white p-3 rounded-lg font-bold hover:bg-blue-700 transition"
         >
           {loading ? "Loading..." : 'Apply Now'}
